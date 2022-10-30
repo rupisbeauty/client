fe32b36df0cc4ac2153ffc56abc8d9fb2aa81639
gshah2020 <gaurang.r.shah@gmail.com>
2022-10-29 23:29:42 -0400

    remove css-modules

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 import { type NextPage } from 'next';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Head from 'next/head';
-import Link from 'next/link';
 import { trpc } from '../utils/trpc';
 
+import { Box, Button, chakra, Flex } from '@chakra-ui/react';
 import { isDev } from '../utils/constants';
-import styles from './index.module.css';
 
 const Home: NextPage = () => {
   const hello = trpc.example.hello.useQuery({ text: 'from tRPC' });
@@ -17,20 +16,54 @@ const Home: NextPage = () => {
         <meta name="description" content="Eyebrows, facials, waxing & more" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className={styles.containerOuter}>
-        <div className={styles.containerInner}>
-          <h1 className={styles.title}>
-            <span className={styles.titlePink}>Rupi&apos;s</span> Beauty Studio
-          </h1>
-          <h2>Coming Soon!</h2>
+      <Flex
+        display="flex"
+        align="center"
+        justify="center"
+        w="full"
+        minH="100vh"
+      >
+        <Flex
+          w="full"
+          maxW={['640px', '768px', '1024px', '1280px', '1536px']}
+          minH="100vh"
+          p={4}
+          direction="column"
+          align="center"
+          justify="center"
+        >
+          <chakra.h1
+            m={0}
+            fontSize={[null, '4xl', '5xl']}
+            fontWeight={800}
+            lineHeight={1.5}
+          >
+            <Box as="span" color="rgba(216, 180, 254, 100%)">
+              Rupi&apos;s
+            </Box>{' '}
+            Beauty Studio
+          </chakra.h1>
+          <chakra.h2 fontWeight={600}>Coming Soon!</chakra.h2>
           {isDev && (
-            <div className={styles.helloFrom}>
-              {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading...</p>}
-            </div>
+            <Flex
+              pt={2}
+              fontSize="md"
+              lineHeight="2rem"
+              color="blue.500"
+              align="center"
+              justify="center"
+              w="full"
+            >
+              {hello.data ? (
+                <chakra.p m={0}>{hello.data.greeting}</chakra.p>
+              ) : (
+                <p>Loading...</p>
+              )}
+            </Flex>
           )}
           <AuthShowcase />
-        </div>
-      </div>
+        </Flex>
+      </Flex>
     </>
   );
 };
@@ -43,42 +76,14 @@ const AuthShowcase: React.FC = () => {
   const { data: sessionData } = useSession();
 
   return (
-    <div className={styles.authShowcase}>
-      {sessionData && <p>Logged in as {sessionData?.user?.name}</p>}
-      {secretMessage && <p>{secretMessage}</p>}
-      <button
-        className={styles.signInButton}
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
+    <Flex m={1} direction="column" align="center" gap={4}>
+      {sessionData && (
+        <chakra.p>Logged in as {sessionData?.user?.name}</chakra.p>
+      )}
+      {secretMessage && <chakra.p>{secretMessage}</chakra.p>}
+      <Button onClick={sessionData ? () => signOut() : () => signIn()}>
         {sessionData ? 'Sign out' : 'Sign in'}
-      </button>
-    </div>
-  );
-};
-
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className={styles.card}>
-      <h2 className={styles.cardTitle}>{name}</h2>
-      <p className={styles.cardDescription}>{description}</p>
-      <Link
-        className={styles.cardDocumentation}
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </Link>
-    </section>
+      </Button>
+    </Flex>
   );
 };
