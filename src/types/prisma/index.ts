b4ea061fef57bb3464a2c1f0674d54d4619297b0
gshah2020 <gaurang.r.shah@gmail.com>
2022-10-30 16:12:54 -0400

    remove trpc example router

--- src/types/prisma/index.ts
+++ src/types/prisma/index.ts
@@ -1,4 +1,4 @@
-import { Prisma, PrismaClient } from '@prisma/client';
+import type { Prisma, PrismaClient } from '@prisma/client';
 
 export type PrismaClientType = PrismaClient<
   Prisma.PrismaClientOptions,
