77a6117a7a1f3a2661a8f941647f3fd35d18006a
gshah2020 <gaurang.r.shah@gmail.com>
2022-10-29 22:14:10 -0400

    fix: next-auth session callback

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,18 +1,18 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import { env } from "../../../env/server.mjs";
-import { prisma } from "../../../server/db/client";
+import { env } from '../../../env/server.mjs';
+import { prisma } from '../../../server/db/client';
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
   callbacks: {
     session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
-      }
+      // if (session.user) {
+      //   session.user.id = user.id;
+      // }
       return session;
     },
   },
