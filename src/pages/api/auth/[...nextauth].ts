0ce0822a0d0a04a1c7ab9bda89612650b9b0a94a
gshah2020 <gaurang.r.shah@gmail.com>
2022-11-01 23:57:45 -0400

    update doclinks sidebar contetn

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,11 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
+import NextAuth, { type NextAuthOptions } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
 // Prisma adapter for NextAuth, optional and can be removed
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 
-import { env } from "@/env/server.mjs";
+import { env } from '@/env/server.mjs';
 
-import {prisma} from "@/server/db/client"
-
-
+import { prisma } from '@/server/db/client';
 
 export const authOptions: NextAuthOptions = {
   // Include user.id on session
@@ -23,6 +21,7 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GoogleProvider({
+      // @TODO: update google oauth using credentials from rupibeautystudio@gmail.com acct.
       clientId: env.GOOGLE_CLIENT_ID,
       clientSecret: env.GOOGLE_CLIENT_SECRET,
     }),
