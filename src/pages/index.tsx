9e112836257850dc4ddc45884df7070c112842f5
gshah2020 <gaurang.r.shah@gmail.com>
2022-11-09 11:41:55 -0500

    refactor sandbox/index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,8 @@
 import { Hero } from '@/components/home';
-import { BRAND_DIR, CDN_URL } from '@/utils';
-import { Box, chakra, Container } from '@chakra-ui/react';
-import { ChakraNextImage, PageLayout, ParallaxItem } from 'chakra.ui';
+import { ComingSoon } from '@/components/home/coming-soon';
+import { PageLayout } from 'chakra.ui';
 import { type NextPage } from 'next';
-import { Background, Parallax } from 'react-parallax';
-import { FramerParallax } from '../../chakra.ui/framer/parallax';
-import { isDev, LOCATIONS_DIR } from '../utils/constants';
+import { isDev } from '../utils/constants';
 
 const insideStyles = {
   background: 'white',
@@ -24,17 +21,7 @@ const Home: NextPage = () => {
         title="Rupi Beauty Studio"
         description="Eyebrows, facials, waxing & more"
       >
-        <FramerParallax>
-          <Initial />
-        </FramerParallax>
-        <Parallax
-          bgImage={`${CDN_URL}${LOCATIONS_DIR}/pearl-river-interior.jpeg`}
-          strength={-100}
-        >
-          <chakra.div height={500}>
-            <chakra.div sx={insideStyles}>Reverse direction</chakra.div>
-          </chakra.div>
-        </Parallax>
+        <ComingSoon />
       </PageLayout>
     </>
   );
@@ -47,31 +34,13 @@ export default Home;
 export const Initial = () => {
   return (
     <>
-      {isDev ? (
+      {!isDev ? (
         <>
           <Hero />
         </>
       ) : (
         <ComingSoon />
       )}
-    </>
-  );
-};
-
-export const ComingSoon = () => {
-  return (
-    <>
-      <Box position="relative" w="20vw" flex={1} height="100vh">
-        <ChakraNextImage
-          width="500"
-          height="430"
-          objectFit="contain"
-          src={`${CDN_URL}${BRAND_DIR}/RUPI-NEW-LOGO-PNG-MD.png`}
-          alt=""
-          priority
-        />
-      </Box>
-      <chakra.p fontSize="4xl">Coming Soon!</chakra.p>
     </>
   );
 };
