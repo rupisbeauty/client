6596401260f65e16a52af47e717e2d72c0147eb3
gshah2020 <gaurang.r.shah@gmail.com>
2022-11-16 15:45:30 -0500

    add all policies

--- src/pages/policies/privacy.tsx
+++ src/pages/policies/privacy.tsx
@@ -1,7 +1,43 @@
+import { FullLogo } from '@/components';
+import { CDN_URL, PEOPLE_DIR } from '@/utils/constants';
+import { Box, chakra, Container, Divider } from '@chakra-ui/react';
 import type { NextPage } from 'next';
+import Image from 'next/image';
+import { PrivacyPolicyBody } from './content/privacy-policy-body';
 
 const Privacy: NextPage = () => {
-  return <div>Privacy Policy</div>;
+  return (
+    <>
+      <Box w="full" h="30vh" mx="auto" overflow="hidden">
+        <Box transform="translateY(-15%)">
+          <Image
+            width="2400"
+            height="1600"
+            src={`${CDN_URL}${PEOPLE_DIR}/girl-looking-tenderly.jpg`}
+            alt="woman getting facial treatment applied | Image by senivpetro on Freepik"
+            priority
+          />
+        </Box>
+      </Box>
+      <Container
+        w="full"
+        maxW="container.lg"
+        p={16}
+        mx="auto"
+        my={24}
+        bg="white"
+        borderRadius="lg"
+        centerContent
+      >
+        <FullLogo />
+        <Divider borderColor="gray.200" my={12} />
+        <chakra.h1 color="primary">Privacy Policy</chakra.h1>
+        <Box textAlign="left">
+          <PrivacyPolicyBody />
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default Privacy;
