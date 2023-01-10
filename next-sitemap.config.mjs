55180c55820f0723a01e1f6bf0005bb898cf5e63
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-10 15:53:57 -0500

    chore: add vercel for sitemap-config

--- next-sitemap.config.mjs
+++ next-sitemap.config.mjs
@@ -3,7 +3,7 @@
 // @TODO: add postbuild script to generate a sitemap
 // @link: https://github.com/iamvishnusankar/next-sitemap
 const NextSitemapConfig = {
-  siteUrl: 'https://rupisbeautystudio.com',
+  siteUrl: process.env?.VERCEL_URL || process.env?.NEXTAUTH_URL,
   generateRobotsTxt: true,
   exclude: ['/sandbox', '/_splash', '/admin/*', '/api/*'],
 };
