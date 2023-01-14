23505c5792f920cdc955d089542597eda3386708
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-14 16:01:44 -0500

    update custom icon

--- chakra.ui/icons/custom-icon.tsx
+++ chakra.ui/icons/custom-icon.tsx
@@ -5,43 +5,43 @@ import type { BoxProps, ChakraProps } from '@chakra-ui/react';
 import type { FC } from 'react';
 
 type CustomIconProps = {
-  icon: string;
   size: string | number | string[] | number[];
+  icon: string;
   color?: string;
   stroke?: string;
 } & BoxProps;
 
+const Icon: FC<CustomIconProps & ChakraProps> = ({
+  color,
+  size,
+  icon,
+  stroke,
+  ...rest
+}) => {
+  return (
+    <Box
+      as="svg"
+      viewBox={paths[icon]?.viewBox}
+      width={size}
+      height={size}
+      fill={color}
+      stroke={stroke}
+      {...rest}
+    >
+      {paths[icon]?.d.map((d: string, i: number) => (
+        <path key={i} d={d} transform={paths[icon]?.transform} />
+      ))}
+    </Box>
+  );
+};
+
 export const CustomIcon: FC<CustomIconProps> = ({
-  icon = 'add',
   size = '1.25rem',
+  icon = 'add',
   color,
   stroke,
   ...rest
 }) => {
-  const Icon: FC<CustomIconProps & ChakraProps> = ({
-    color,
-    size,
-    icon,
-    stroke,
-    ...rest
-  }) => {
-    return (
-      <Box
-        as="svg"
-        viewBox={paths[icon]?.viewBox}
-        width={size}
-        height={size}
-        fill={color}
-        stroke={stroke}
-        {...rest}
-      >
-        {paths[icon]?.d.map((d: string, i: number) => (
-          <path key={i} d={d} transform={paths[icon]?.transform} />
-        ))}
-      </Box>
-    );
-  };
-
   return (
     <Icon color={color} size={size} icon={icon} stroke={stroke} {...rest} />
   );
