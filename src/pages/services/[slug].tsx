919042ef043e0e3fa28f48f8edbe29c6c53d29ba
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-17 14:32:20 -0500

    UI: update service category page layout

--- src/pages/services/[slug].tsx
+++ src/pages/services/[slug].tsx
@@ -1,5 +1,12 @@
-import { Box, Container, HStack, Text } from '@chakra-ui/react';
-import Image from 'next/image';
+import {
+  Box,
+  Container,
+  Heading,
+  HStack,
+  SimpleGrid,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
 
 import type {
   ServiceContent,
@@ -10,9 +17,9 @@ import type {
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 
 import { PageLayout, SectionTitle } from '@/components';
-import { CustomIcon } from 'chakra.ui';
+import { ChImage, CustomIcon } from 'chakra.ui';
 
-import { slugify } from '@/utils';
+import { capitalize, chunkArray, slugify } from '@/utils';
 import { getImages } from '@/_content';
 
 const ServiceCategory: NextPage<{
@@ -22,66 +29,115 @@ const ServiceCategory: NextPage<{
 }> = ({ services, category, content }) => {
   const icon = category === 'other' ? 'add' : category;
 
+  const chunkedServices = chunkArray(services, 2);
+
   const { cover, accents } = getImages(category as keyof ServiceImages);
 
   return (
-    <PageLayout title={category} subtitle="Rupi Beauty Studio" showCta>
+    <PageLayout
+      title={capitalize(category)}
+      subtitle="Rupi Beauty Studio"
+      showCta
+      showReviews
+    >
       <SectionTitle title={content.title} />
       <Container as="section" layerStyle="container" p={0}>
         <Box w="full" maxH="36vh" overflow="hidden" borderRadius="md">
-          <Image
+          <ChImage
             src={cover.src}
             width={cover.width}
             height={cover.height}
-            alt="Beautiful woman | Image by valuavitaly on Freepik"
+            alt={cover.alt + ' | ' + cover.attr}
+            {...cover.styles}
           />
         </Box>
       </Container>
       <Container as="section" layerStyle="container" centerContent>
-        <HStack>
-          <Box
-            w="36"
-            _before={{
-              content: '""',
-              w: '32',
-              h: '32',
-              bg: 'green.200',
-              position: 'absolute',
-              zIndex: -1,
-              borderRadius: 'full',
-            }}
-          >
-            <CustomIcon icon={icon} size={24} m={4} fill="gray.600" p={2} />
-          </Box>
-          <Box p={12}>
-            {content.description.map((paragraph) => {
-              return (
-                <Text
-                  key={slugify(paragraph.substring(0, 8))}
-                  mb={6}
-                  fontSize="lg"
-                >
-                  {paragraph}
-                </Text>
-              );
-            })}
-          </Box>
-        </HStack>
-        {services.map((service) => {
-          return (
+        <>
+          <HStack>
             <Box
-              key={service.title}
-              w="full"
-              maxH="36vh"
-              overflow="hidden"
-              layerStyle="border"
-              my={2}
-              p={2}
+              w="36"
+              _before={{
+                content: '""',
+                w: '32',
+                h: '32',
+                bg: 'green.200',
+                position: 'absolute',
+                zIndex: -1,
+                borderRadius: 'full',
+              }}
             >
-              {JSON.stringify(service)}
+              <CustomIcon icon={icon} size={24} m={4} fill="gray.600" p={2} />
             </Box>
-          );
-        })}
+            <Box p={12}>
+              {content.description.map((paragraph) => {
+                return (
+                  <Text
+                    key={slugify(paragraph.substring(0, 8))}
+                    mb={6}
+                    fontSize="lg"
+                  >
+                    {paragraph}
+                  </Text>
+                );
+              })}
+            </Box>
+          </HStack>
+          {chunkedServices.map((chunk, i) => {
+            return (
+              <VStack key={`chunk-${i}`}>
+                <Box
+                  w="full"
+                  maxH="28vh"
+                  my={12}
+                  overflow="hidden"
+                  borderRadius="md"
+                >
+                  <ChImage
+                    src={String(accents[i]?.src)}
+                    alt={`${accents[i]?.alt} | ${accents[i]?.attr}`}
+                    width={accents[i]?.width}
+                    height={accents[i]?.height}
+                    {...accents[i]?.styles}
+                    // objectFit="cover"
+                  />
+                </Box>
+                <SimpleGrid
+                  w="full"
+                  overflow="hidden"
+                  columns={[1, 2, 3]}
+                  gap={6}
+                >
+                  {chunk.map((service) => {
+                    return (
+                      <Box
+                        w="full"
+                        p={6}
+                        bg="white"
+                        rounded="lg"
+                        gap={3}
+                        key={service.title}
+                      >
+                        <Heading as="h2" my={6} textTransform="capitalize">
+                          {service.title}
+                        </Heading>
+                        <Text
+                          key={slugify(service.description[0].substring(0, 8))}
+                          py={2}
+                          fontSize="lg"
+                          fontWeight="normal"
+                          lineHeight={1.6}
+                        >
+                          {service.description[0]}
+                        </Text>
+                      </Box>
+                    );
+                  })}
+                </SimpleGrid>
+              </VStack>
+            );
+          })}
+        </>
       </Container>
     </PageLayout>
   );
--- src/utils/fns.ts
+++ src/utils/fns.ts
@@ -147,3 +147,11 @@ export function slugify(str: string) {
 export function capitalize(str: string) {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function isEven(n: number) {
+  return n % 2 == 0;
+}
+
+export function isOdd(n: number) {
+  return Math.abs(n % 2) == 1;
+}
