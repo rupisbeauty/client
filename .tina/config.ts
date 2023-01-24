d3033ee3f45366113f3a53c635fef38a35dd494d
gshah2020 <gaurang.r.shah@gmail.com>
2023-01-23 19:50:12 -0500

    init tinacms example

---
+++ .tina/config.ts
@@ -0,0 +1,52 @@
+import { defineConfig } from 'tinacms';
+
+// Your hosting provider likely exposes this as an environment variable
+const branch =
+  process.env.HEAD ||
+  process.env.NEXT_PUBLIC_TINA_BRANCH || // @TODO: Must add this variable to vercel
+  process.env.VERCEL_GIT_COMMIT_REF ||
+  'main';
+
+export default defineConfig({
+  branch,
+  clientId: String(process.env.NEXT_PUBLIC_TINA_CLIENT_ID), // Get this from tina.io
+  token: String(process.env.NEXT_PUBLIC_TINA_READ_ONLY_TOKEN), // Get this from tina.io
+  build: {
+    outputFolder: 'admin',
+    publicFolder: 'public',
+  },
+  media: {
+    tina: {
+      mediaRoot: '',
+      publicFolder: 'public',
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: 'post',
+        label: 'Posts',
+        path: 'content/posts',
+        fields: [
+          {
+            type: 'string',
+            name: 'title',
+            label: 'Title',
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: 'rich-text',
+            name: 'body',
+            label: 'Body',
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+    ],
+  },
+});
