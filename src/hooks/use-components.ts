db4d8d12230ad5e4cee16bf1e964968f017ff026
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-02 03:17:50 -0500

    fix: use-components hook

--- src/hooks/use-components.ts
+++ src/hooks/use-components.ts
@@ -1,3 +1,5 @@
+import { Divider } from '@chakra-ui/react';
+
 import {
   About,
   ContactInfo,
@@ -23,11 +25,14 @@ export function useComponents(type: 'section' | 'page') {
       serviceMenu: ServiceMenu,
       box: SectionBox,
       logo: FullLogo,
+      divider: Divider,
     },
     page: {
+      logo: FullLogo,
       about: About,
       hero: Hero,
       sectionTitle: SectionTitle,
+      divider: Divider,
       locationCTA: VisitLocationCTA,
       contact: ContactInfo,
       coreServices: CoreServices,
