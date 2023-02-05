8ee228f2b17221463143dade70b1c7a78801178d
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-05 00:02:59 -0500

    prep for v1 > v2 migration

---
+++ src/pages/services/[...service].tsx
@@ -0,0 +1,47 @@
+import type {
+  GetStaticPaths,
+  GetStaticProps,
+  InferGetStaticPropsType,
+} from 'next';
+import { useTina } from 'tinacms/dist/react';
+
+import { PageLayout } from '@/components';
+
+import { serviceCategorySchema } from '.tina/validators';
+import {
+  getCurrentServiceTinaProps,
+  getServicesTinaPaths,
+} from '.tina/_data-fetchers/services';
+import { ServiceBlocks } from '@/components/blocks/templates/service-blocks';
+
+type ServicePageProps = InferGetStaticPropsType<typeof getStaticProps>['props'];
+
+const TinaSandbox: React.FC<ServicePageProps> = (props) => {
+  const { data } = useTina({
+    query: props.query,
+    variables: props.variables,
+    data: props.data,
+  });
+
+  // @FIXME: update zod schema
+  // const service = serviceCategorySchema.parse(data?.categories);
+  return data?.categories ? (
+    <PageLayout {...data?.categories}>
+      <ServiceBlocks {...data?.categories} />
+    </PageLayout>
+  ) : (
+    <>Error loading markdown</>
+  );
+};
+
+export default TinaSandbox;
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: await getServicesTinaPaths(),
+  fallback: 'blocking',
+});
+
+export const getStaticProps: GetStaticProps = async (context) => ({
+  props: await getCurrentServiceTinaProps(context),
+  revalidate: 300000,
+});
