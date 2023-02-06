b95fffe3902f7ee38b8717a6b83b0d9646ee520c
gshah2020 <gaurang.r.shah@gmail.com>
2023-02-06 10:16:02 -0500

    chore: remove last data remnants from v1

--- src/_content/services/category-color-scheme.ts
+++
@@ -1,9 +0,0 @@
-import type { Services } from '@/_content/';
-
-export const categoryColorScheme: Record<keyof Services, string> = {
-  all: 'purple',
-  eyebrows: 'teal',
-  facials: 'blue',
-  waxing: 'green',
-  other: 'red',
-};
